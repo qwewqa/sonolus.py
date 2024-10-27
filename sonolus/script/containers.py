1b8a717da33b9557bf46d07d1a724c89185d7a30
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2024-10-26 23:23:45 -0700

    Set ops

--- sonolus/script/containers.py
+++ sonolus/script/containers.py
@@ -1,7 +1,8 @@
 from sonolus.script.array import Array
-from sonolus.script.debug import assert_true
 from sonolus.script.iterator import ArrayLike
+from sonolus.script.range import Range
 from sonolus.script.record import Record
+from sonolus.script.values import copy
 
 
 class VarArray[T, Capacity](Record, ArrayLike[T]):
@@ -18,9 +19,54 @@ class VarArray[T, Capacity](Record, ArrayLike[T]):
         self._array[key] = value
 
     def append(self, value: T):
-        assert_true(self._size < self._array.size())
+        """Appends a copy of the given value to the end of the array."""
+        assert self._size < self._array.size()
         self._array[self._size] = value
         self._size += 1
 
+    def pop(self, index: int) -> T:
+        """Removes and returns a copy of the value at the given index."""
+        assert 0 <= index < self._size
+        value = copy(self._array[index])
+        self._size -= 1
+        if index < self._size:
+            for i in Range(index, self._size):
+                self._array[i] = self._array[i + 1]
+        return value
+
+    def remove(self, value: T) -> bool:
+        """Removes the first occurrence of the given value, returning whether the value was removed.
+
+        Preserves the relative order of the elements.
+        """
+        index = self.index_of(value)
+        if index < 0:
+            return False
+        self.pop(index)
+        return True
+
     def clear(self):
+        """Sets size to zero."""
         self._size = 0
+
+    def set_add(self, value: T) -> bool:
+        """Adds a copy of the given value if it is not already present, returning whether the value was added."""
+        if self._size >= self._array.size():
+            return False
+        if value in self:
+            return False
+        self.append(value)
+        return True
+
+    def set_remove(self, value: T) -> bool:
+        """Removes the first occurrence of the given value, returning whether the value was removed.
+
+        Does not preserve the relative order of the elements.
+        """
+        index = self.index_of(value)
+        if index < 0:
+            return False
+        if index < self._size - 1:
+            self._array[index] = self._array[self._size - 1]
+        self._size -= 1
+        return True
