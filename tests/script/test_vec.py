94c197d1f069f636ac67d80ae131dfd7dbf2df59
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2024-11-14 15:05:46 -0800

    Add Vec2 tests

---
+++ tests/script/test_vec.py
@@ -0,0 +1,129 @@
+from math import pi
+
+from hypothesis import strategies as st
+
+from sonolus.script.vec import Vec2
+from tests.script.conftest import is_close, validate_dual_run
+
+floats = st.floats(min_value=-999, max_value=999, allow_nan=False, allow_infinity=False)
+nonzero_floats = floats.filter(lambda x: abs(x) > 1e-2)
+
+
+def test_magnitude():
+    def fn():
+        v = Vec2(3, 4)
+        return v.magnitude
+
+    assert validate_dual_run(fn) == 5
+
+
+def test_angle():
+    def fn():
+        v = Vec2(1, 1)
+        return v.angle
+
+    assert is_close(validate_dual_run(fn), 0.25 * pi)
+
+
+def test_dot():
+    def fn():
+        v = Vec2(1, 2)
+        u = Vec2(3, 4)
+        return v.dot(u)
+
+    assert validate_dual_run(fn) == 11
+
+
+def test_rotate():
+    def fn():
+        v = Vec2(1, 1)
+        return v.rotate(0.25 * pi)
+
+    assert validate_dual_run(fn) == Vec2(0, 2**0.5)
+
+
+def test_rotate_about():
+    def fn():
+        v = Vec2(1, 1)
+        pivot = Vec2(1, 0)
+        return v.rotate_about(0.5 * pi, pivot)
+
+    assert is_close(validate_dual_run(fn).magnitude, 0)
+
+
+def test_add():
+    def fn():
+        v = Vec2(1, 2)
+        u = Vec2(3, 4)
+        return v + u
+
+    assert validate_dual_run(fn) == Vec2(4, 6)
+
+
+def test_sub():
+    def fn():
+        v = Vec2(1, 2)
+        u = Vec2(3, 4)
+        return v - u
+
+    assert validate_dual_run(fn) == Vec2(-2, -2)
+
+
+def test_mul_vec():
+    def fn():
+        v = Vec2(1, 2)
+        u = Vec2(3, 4)
+        return v * u
+
+    assert validate_dual_run(fn) == Vec2(3, 8)
+
+
+def test_mul_float():
+    def fn():
+        v = Vec2(1, 2)
+        return v * 3
+
+    assert validate_dual_run(fn) == Vec2(3, 6)
+
+
+def test_div_vec():
+    def fn():
+        v = Vec2(1, 2)
+        u = Vec2(3, 4)
+        return v / u
+
+    assert validate_dual_run(fn) == Vec2(1 / 3, 2 / 4)
+
+
+def test_div_float():
+    def fn():
+        v = Vec2(1, 2)
+        return v / 3
+
+    assert validate_dual_run(fn) == Vec2(1 / 3, 2 / 3)
+
+
+def test_neg():
+    def fn():
+        v = Vec2(1, 2)
+        return -v
+
+    assert validate_dual_run(fn) == Vec2(-1, -2)
+
+
+def test_equal():
+    def fn():
+        v = Vec2(1, 2)
+        u = Vec2(1, 2)
+        return v == u
+
+    assert validate_dual_run(fn)
+
+
+def test_not_equal():
+    def fn():
+        v = Vec2(1, 2)
+        u = Vec2(3, 4)
+        return v != u
+
+    assert validate_dual_run(fn)
