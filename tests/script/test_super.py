a93621c3a030174d76135b2912c9aad90204f5b8
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2025-08-06 12:22:57 -0700

    Add classmethod tests for super

--- tests/script/test_super.py
+++ tests/script/test_super.py
@@ -7,11 +7,21 @@ class A:
     def m(self):
         debug_log(1)
 
+    @classmethod
+    def m_cls(cls):
+        debug_log(1)
+
 
 class B(A):
     def m(self):
         debug_log(2)
         super().m()
+        debug_log(3)
+
+    @classmethod
+    def m_cls(cls):
+        debug_log(2)
+        super().m_cls()
         debug_log(3)
 
 
@@ -21,11 +31,23 @@ class C(A):
         super().m()
         debug_log(5)
 
+    @classmethod
+    def m_cls(cls):
+        debug_log(4)
+        super().m_cls()
+        debug_log(5)
+
 
 class D(B, C):
     def m(self):
         debug_log(6)
         super().m()
+        debug_log(7)
+
+    @classmethod
+    def m_cls(cls):
+        debug_log(6)
+        super().m_cls()
         debug_log(7)
 
 
@@ -45,5 +67,25 @@ def test_super_diamond():
 
     def fn():
         d.m()
+
+    run_and_validate(fn)
+
+
+def test_super_classmethod():
+    b = B()
+    b._is_comptime_value_ = True  # type: ignore
+
+    def fn():
+        b.m_cls()
+
+    run_and_validate(fn)
+
+
+def test_super_classmethod_diamond():
+    d = D()
+    d._is_comptime_value_ = True  # type: ignore
+
+    def fn():
+        d.m_cls()
 
     run_and_validate(fn)
