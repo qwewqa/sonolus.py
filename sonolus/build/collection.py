122d0341c5ca8ee08a5bd2e13cff3f8f3c3a7833
qwewqa <198e559dbd446d973355f415bdfa34@gmail.com>
2025-10-12 17:52:20 -0700

    Warn on invalid item.json

--- sonolus/build/collection.py
+++ sonolus/build/collection.py
@@ -4,6 +4,7 @@ import gzip
 import hashlib
 import json
 import urllib.request
+import warnings
 import zipfile
 from io import BytesIO
 from os import PathLike
@@ -116,6 +117,7 @@ class Collection:
                 try:
                     item_data = json.loads(item_json_path.read_text(encoding="utf-8"))
                 except json.JSONDecodeError:
+                    warnings.warn(f"Invalid JSON in {item_json_path}, skipping item.")
                     continue
 
                 item_data = self._localize_item(item_data)
